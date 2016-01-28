651014e19cb6aa763bd51c0ce66619dfd4f6294f
CKiilu <cndeti@gmail.com>
2016-01-29 01:46:38 +0300

    change animaitopmd

---
+++ start/js/warsaw.js
@@ -0,0 +1,7 @@
+var mapOptions = {
+    center: new google.maps.LatLng(37.52.2272843, 20.9961045),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+new google.maps.Map(document.getElementById('locationOptions);
