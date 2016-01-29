34ca8d410d51d6864f82bbe8a614381b24edc1cc
CKiilu <cndeti@gmail.com>
2016-01-29 07:58:12 +0300

    about overlay and warsaw.js del

--- start/js/warsaw.js
+++
@@ -1,7 +0,0 @@
-var mapOptions = {
-    center: new google.maps.LatLng(52.2272843, 20.9961045),
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-
-new google.maps.Map(document.getElementById('location');
